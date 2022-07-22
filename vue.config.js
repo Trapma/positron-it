ef955bd3f74214a2365e7ec0e24770c577a994fb
Mikhail Trapeznikov <trapma@yandex.ru>
2022-07-22 18:45:27 +1000

    set and connect scss

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+        @import "~@/scss/variables.scss";
+        @import "~@/scss/mixins.scss";
+        `,
+      },
+    },
+  },
 });
