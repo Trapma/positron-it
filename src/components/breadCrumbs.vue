26d9f6747a89d7e063cd5b6221996b75bbf8e0c0
Mikhail Trapeznikov <trapma@yandex.ru>
2022-07-24 01:39:27 +1000

    create breadCrumbs

---
+++ src/components/breadCrumbs.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="crumbs">
+    <span class="crumbs__start">Главная</span>
+    <img class="crumbs__arrow" src="@/assets/image/svg/vector.svg" />
+    <span class="crumbs__end">Корзина</span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.crumbs {
+  display: flex;
+  margin-top: 31px;
+
+  &__arrow {
+    margin: 0 10px;
+  }
+
+  &__start {
+    @include text-lato(normal, 400, 14px, 21px, $text-crumbs-start-color);
+  }
+  &__end {
+    @include text-lato(normal, 400, 14px, 21px, $text-thin-color);
+  }
+}
+</style>
