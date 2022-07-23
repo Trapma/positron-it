adb919e290f4f4da15314d6e9f76cb03a36ab0e8
Mikhail Trapeznikov <trapma@yandex.ru>
2022-07-24 01:40:58 +1000

    create CartPage

---
+++ src/views/CartPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <nav-bar />
+    <bread-crumbs />
+    <cart-item-info />
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import NavBar from "@/components/NavBar.vue";
+import BreadCrumbs from "@/components/breadCrumbs.vue";
+import CartItemInfo from "@/components/CartItemInfo.vue";
+export default {
+  components: {
+    NavBar,
+    BreadCrumbs,
+    CartItemInfo,
+  },
+  mounted() {},
+  ...mapState({
+    cartItem: (state) => state.cart.cartItems,
+  }),
+};
+</script>
+<style scoped></style>
