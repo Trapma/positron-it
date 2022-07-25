bb6b508bea4d06c22d65e5fa4553da287abc7027
Mikhail Trapeznikov <trapma@yandex.ru>
2022-07-26 02:12:05 +1000

    create checkbox Ui

---
+++ src/components/Ui/checkbox/CCheckbox.vue
@@ -0,0 +1,48 @@
+<template>
+  <label>
+    <input type="checkbox" @change="$emit('input', $event.target.checked)" />
+    <span></span>
+  </label>
+</template>
+<script>
+export default {
+  name: "c-checkbox",
+};
+</script>
+<style lang="scss" scoped>
+:root {
+  label {
+    position: relative;
+  }
+  span {
+    width: 20px;
+    height: 20px;
+    border: 1px solid #797b86;
+    box-shadow: inset 2px 2px 1px rgba(0, 0, 0, 0.08);
+    border-radius: 2px;
+    display: inline-block;
+    transition: all linear 0.3s;
+    &:after {
+      content: "";
+      position: absolute;
+      top: -5px;
+      left: 7px;
+      border-bottom: 2px solid #0069b4;
+      border-right: 2px solid #0069b4;
+      height: 11px;
+      width: 6px;
+      transform: rotate(45deg);
+      visibility: hidden;
+    }
+  }
+  input {
+    display: none;
+    &:checked ~ span {
+      background: rgb(255, 255, 255);
+      &:after {
+        visibility: visible;
+      }
+    }
+  }
+}
+</style>
