16fbb99302c19f97fdfa9ea5ef6ddf9997009526
Mikhail Trapeznikov <trapma@yandex.ru>
2022-07-26 15:00:17 +1000

    create orderPage

---
+++ src/views/OrderPage.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="wrapper">
+    <nav-bar />
+    <bread-crumbs />
+    <div class="cart-wrapper">
+      <div class="cart-info"><cart-item-info /></div>
+      <div class="cart-order"><cart-item-order /></div>
+    </div>
+    <div class="viewed-wrapper">
+      <div class="cart-viewed"><viewed-products /></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from "vuex";
+import NavBar from "@/components/NavBar.vue";
+import BreadCrumbs from "@/components/breadCrumbs.vue";
+import CartItemInfo from "@/components/CartItemInfo.vue";
+import CartItemOrder from "@/components/CartItemOrder.vue";
+import ViewedProducts from "@/components/ViewedProducts.vue";
+
+export default {
+  components: {
+    NavBar,
+    BreadCrumbs,
+    CartItemInfo,
+    CartItemOrder,
+    ViewedProducts,
+  },
+  mounted() {
+    console.log(this.$route.params);
+    // eslint-disable-next-line no-undef
+    this.getOrderPay(this.$route.params.id);
+  },
+  computed: {
+    ...mapState({
+      cartItem: (state) => state.cart.cartItems,
+      isLoading: (state) => state.cart.isLoading,
+    }),
+  },
+  methods: {
+    ...mapActions("cart", ["getOrderPay"]),
+  },
+};
+</script>
+<style scoped>
+.wrapper {
+  width: 1280px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.cart-wrapper {
+  width: 100%;
+  display: flex;
+  margin-top: 30px;
+  justify-content: space-between;
+}
+.viewed-wrapper {
+  margin-top: 84px;
+}
+.cart-info {
+  min-width: 800px;
+}
+.cart-order {
+  margin-left: 55px;
+  margin-top: 118px;
+  min-width: 425px;
+}
+</style>
